dadba9b4df9c4c593dd0ed582d06e22a9b2ffda8
raf <raf@example.com>
2022-10-20 00:14:48 +0400

    Added tasks 1 and 2

--- Homeworks1/Program.cs
+++ Homeworks1/Program.cs
@@ -1,2 +1,53 @@
-﻿// Задача №1
+﻿// Задача №1 Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+/*
+
+Console.Write("Input first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber == secondNumber)
+    Console.Write("The first and second numbers are equal");
+
+if(firstNumber > secondNumber)
+    Console.Write("The first number is greater than the second");
+
+if(firstNumber < secondNumber)
+    Console.Write("The second number is greater than the first");
+
+*/
+
+
+
+// Задача №2 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*
+
+Console.Write("Input first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input third number: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+int max = thirdNumber;
+
+if(max < firstNumber)
+    max = firstNumber;
+
+if(max < secondNumber)
+    max = secondNumber;
+
+if(max == firstNumber || firstNumber == secondNumber)
+    Console.WriteLine("The numbers are equal");
+
+Console.Write($"The maximum number is {max}");
+
+
+*/
+
 
